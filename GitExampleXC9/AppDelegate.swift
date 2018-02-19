25f63d8fceedea2494657fd0c1445a16ade47f1a
Jarod Stephenson <jarodstephenson@live.com>
2018-02-19 00:15:40 -0500

    Initial Commit

---
+++ GitExampleXC9/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitExampleXC9
+//
+//  Created by Jarod Stephenson on 2/19/18.
+//  Copyright © 2018 Jarod Stephenson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitExampleXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExampleXC9
+//
+//  Created by Jarod Stephenson on 2/19/18.
+//  Copyright © 2018 Jarod Stephenson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
